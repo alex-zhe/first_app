181b09f62efbc0f7de3552f67edf3263404abc4b
alex-zhe <azheka@ukr.net>
2012-07-29 23:12:45 +0300

    first

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-First::Application.config.secret_token = 'cfe594d3561b017513ce099260487ab2a7612cf8a92550aa3db85b881e6b2ff82c8b7cc98610293353d1b526daafd9e4f7f145e8da4a67d1ddd28dd4dcc3eed3'
+First::Application.config.secret_token = 'b3e0d6015e500dfbcca6494b733cfc3b4405f745e1ea5518c33216c0fc891f0252198107e323563c20bba2f48cee43f4d0b4b38781c7d0ffa46c7177d09f2cb2'
